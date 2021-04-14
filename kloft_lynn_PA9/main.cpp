2e9ddf557dfdc0fbac9cba3ba3a53ec7c99725a3
Logan Kloft <lkloft@outlook.com>
2021-04-14 13:34:39 -0700

    Added GameLoop and pollEvent loop

--- kloft_lynn_PA9/main.cpp
+++ kloft_lynn_PA9/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
@@ -6,5 +8,30 @@
 
 int main(void)
 {
+	sf::RenderWindow window(sf::VideoMode(1480, 1080), "Tower Defense", sf::Style::Close);
+	sf::CircleShape range(40);
+	range.setFillColor(sf::Color::Red);
+	while (window.isOpen())
+	{
+		window.clear();
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+		{
+			range.setPosition(sf::Mouse::getPosition(window).x - range.getRadius(), sf::Mouse::getPosition(window).y - range.getRadius());
+			window.draw(range);
+		}
+
+		window.display();
+	}
+
 	return 0;
 }
