a20a54590a1eb6771dff82de8758229415b62107
Jeremiah Lynn <jeremiah.lynn@wsu.edu>
2021-04-15 09:49:21 -0700

    Commit to main.

---
+++ kloft_lynn_PA9/main.cpp
@@ -0,0 +1,25 @@
+/************************************************
+*	Creators: Jeremiah Lynn and Logan Kloft		*
+*	Project: #TBD# Defense Game					*
+*	Class: CPT_S 122							*
+*	Description: just a game made for class		*
+*				 that will one day sell for		*
+*				 millions of dollars.. prolly	*
+*												*
+************************************************/
+
+/*
+*	Last Commit	: 4/15/21 9:47AM
+*	Commit by:	Jeremiah
+*	Description: Added comment block and possible
+*				 format for making and commenting
+*				 commits.
+*/
+
+int main(void)
+{
+
+
+
+	return 0;
+}
