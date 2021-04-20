0d3ed2c68572b5989b79e1f0bd7406a8e92ceb24
Jeremiah Lynn <jeremiah.lynn@wsu.edu>
2021-04-20 13:47:39 -0700

    Added Jeremiah's Paths for SFML

--- kloft_lynn_PA9/main.cpp
+++ kloft_lynn_PA9/main.cpp
@@ -16,8 +16,51 @@
 *				 commits.
 */
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+
 int main(void)
 {
+	sf::RenderWindow window(sf::VideoMode(1440, 1080, 32), "SFML works!", sf::Style::Close);
+
+	sf::RectangleShape shape({ 100,100 });
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && shape.getPosition().x > 0.5)
+		{
+			shape.move({ -0.5,0.0 });
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && shape.getPosition().x + shape.getSize().x < 1440)
+		{
+			shape.move({ 0.5,0.0 });
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && shape.getPosition().y + shape.getSize().y < 1080)
+		{
+			shape.move({ 0.0,0.5 });
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && shape.getPosition().y > 0.5)
+		{
+			shape.move({ 0.0,-0.5 });
+		}
+
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 
 
 
